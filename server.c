aacc28915fec57e7cd1c4a9a6b55a7b08731edaf
hunter666hz <hzhunter7@gmail.com>
2024-01-20 22:52:47 +0100

    modified:   server.c

--- server.c
+++ server.c
@@ -11,13 +11,14 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include"lib/common.h"
-typedef enum
-{
+#include<stdbool.h>
+// typedef enum
+// {
 
-    false,
-    true
-}
-bool;
+//     false,
+//     true
+// }
+// bool;
 void exitt(int clisocket){
     
 }
@@ -26,7 +27,9 @@ typedef struct{
     int clisocket;
     int clientlenght;
     struct sockaddr_in client_addres;
-    bool err;
+    boolean isonline ;
+    boolean err;
+    
 }supcli;
 typedef struct clilist{
     supcli* val;
@@ -35,23 +38,25 @@ typedef struct clilist{
 clilist *head = NULL;
 #define letzero(s) (void)memset(s, 0, sizeof(s));
 int client_socket,sock ;
+
+
 void deleteclient(supcli* cli){
     clilist* a = head->next ;
-    clilist* b = head ; 
+        clilist* b = head ; 
     if(b->val == cli ){
         a = b;
-        b = b->next;
-        free(a);
+            b = b->next;
+                free(a);
     }
     else{
 
     while(a->val != cli && a->next != NULL){
         a = a->next;
-        b = b->next;
+            b = b->next;
     }
     b->next = a->next ;
-    a->next = NULL;
-    free(a);
+        a->next = NULL;
+            free(a);
     }
 }
 void clientlist(clilist**thead,supcli* client ){
@@ -63,17 +68,19 @@ void clientlist(clilist**thead,supcli* client ){
     }
 
 
-    p->val = client;
-    p->next = *thead;
-    *thead = p;
+        p->val = client;
+            p->next = *thead;
+                *thead = p;
 }
+
+
 void resend(int clisocket,char* buffer){
     clilist *p = head;
     while(p!=NULL){
         if(p->val->clisocket != clisocket){
-            send(p->val->clisocket, buffer, strlen(buffer)+1, 0);
+                send(p->val->clisocket, buffer, strlen(buffer)+1, 0);
         }
-        p = p->next;
+                        p = p->next;
     }
     //printf("%s\n", buffer);
     //printf("admin : ");
@@ -86,65 +93,90 @@ void resend(int clisocket,char* buffer){
         {
 
             send(p->val->clisocket, mes, strlen(mes)+1, 0);
-            p = p->next;
+                p = p->next;
     }
     }
+
+
+
+
 DWORD WINAPI spcread(LPVOID thrd){
     int client_socket = *((int *)thrd);
     char mes[1024];
    do{
     letzero(mes);
-    recv(client_socket, mes, sizeof(mes), 0);
-    resend(client_socket, mes);
-    printf("\r%s\n", mes);
-    printf("admin : ");
-   } while (isStringeq(mes,"exit"));
+        recv(client_socket, mes, sizeof(mes), 0);
+            resend(client_socket, mes);
+                printf("\r%s\n", mes);
+                    printf("admin : ");
+   } while (!isStringeq(mes,"exit"));
+    
 }
+
+
+
 
 DWORD WINAPI spcaccept(LPVOID thrd){
 
    while(true){
     supcli *client = (supcli*)malloc(sizeof(supcli));
-    client->clientlenght = sizeof(client->client_addres);
-    client->clisocket = accept(sock, (struct sockaddr *)&client->client_addres, &client->clientlenght);
-    recv(client->clisocket, client->name, sizeof(client->name), 0);
-    // strcpy(recv(client->clisocket , ),client->name );
-    clientlist(&head, client);
-    printf("%s\n", client->name);
+        client->clientlenght = sizeof(client->client_addres);
+            client->clisocket = accept(sock, (struct sockaddr *)&client->client_addres, &client->clientlenght);
+                recv(client->clisocket, client->name, sizeof(client->name), 0);
+                // client->isonline = true;
+                // strcpy(recv(client->clisocket , ),client->name );
+                // printf("%s\n", client->name);
+                clientlist(&head, client);
 
-    HANDLE threade;
-    threade = CreateThread(NULL, 0, spcread, (LPVOID)&client->clisocket, 0, NULL);
-        if (threade = NULL)
-    {
-        fprintf(stderr, "404 err hapend ... \n");
-    }
+                HANDLE threade;
+                threade = CreateThread(NULL, 0, spcread, (LPVOID)&client->clisocket, 0, NULL);
+                if (threade = NULL)
+                {
+                    fprintf(stderr, "404 err hapend ... \n");
+            }
+
     WaitForSingleObject( threade,  INFINITE);
-    close(client->clisocket);
+        close(client->clisocket);
 
     }
 }
+
+
+
+
+
+
 DWORD WINAPI  spcwrite(LPVOID thrd){
 do{
-
     clilist *p = head;
     char messa[1024];
     char buffer[1024];
+
     letzero(buffer);
-    printf("admin : " );
-    fgets(buffer, sizeof(buffer), stdin);
-    strtok(buffer, "\n");
-    sprintf(messa, "admin : %s", buffer);
-    strtok(messa, "\n");
-    justsned( messa);
-} while (head);
+        printf("admin : " );
+            fgets(buffer, sizeof(buffer), stdin);
+                strtok(buffer, "\n");
+                    sprintf(messa, "admin : %s", buffer);
+                        strtok(messa, "\n");
+                            justsned( messa);
+} while (head!= NULL);
 }
+
+
+
+
+
+
+
+
+
 
 int main(){
  WSADATA wsaData; // Windows socket initialization data
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
     {
         fprintf(stderr, "Failed to initialize Winsock\n");
-        return 1;
+            return 1;
     }
 
 
@@ -156,11 +188,11 @@ int main(){
     int client_length; // Change the type from socklen_t to int
     printf("[+] server is on listen mode \n");
 
-    sock = socket(AF_INET, SOCK_STREAM, 0);
+        sock = socket(AF_INET, SOCK_STREAM, 0);
     if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (const char *)&opval, sizeof(opval)) < 0)
     {
         printf("[-] some problem in tcp protocol connection\n");
-        return 1;
+            return 1;
     }
 
     server_address.sin_family = AF_INET;
